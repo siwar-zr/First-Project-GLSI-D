4063b1fde43d9ac829ae25e591ff20a349288d17
siwar-zr <siwarzrelli180959@gmail.com>
2025-02-20 15:54:32 +0100

    ngStyle and Routing

--- src/app/components/data-binding/data-binding.component.ts
+++ src/app/components/data-binding/data-binding.component.ts
@@ -1,22 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.component.html',
   styleUrl: './data-binding.component.css'
 })
 export class DataBindingComponent {
-
-  firstName: string = "Rim";
+  firstName: string = "Mohamed";
   annee: number = 2025;
   isActive: boolean = true;
   currentDate: Date = new Date();
+  myPlaceHolder: string = "Entrez votre nom";
+  divClassName: string = "text-bg-primary p-3";
 
   constructor(){
     console.log(this.firstName);
     this.isActive = false;
     console.log(this.isActive);
+  }
+
+  showWelcomeMessage(){
+    alert("Welcome to our Angular Course!");
+  }
+
+  onCityChange(){
+    console.log("City changed");
   }
 
 }
--- src/app/components/ng-class/ng-class.component.ts
+++ src/app/components/ng-class/ng-class.component.ts
@@ -1,6 +1,7 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ng-class',
@@ -14,7 +15,16 @@ export class NgClassComponent {
   isChecked: boolean = false;
   div3ClassName: string ='';
 
+  constructor(private router: Router){
+
+  }
+
   addDiv1Color(className: string){
     this.divBgColor = className;
   }
+
+  navigateToDB(){
+    this.router.navigateByUrl("/dataBinding")
+  }
+
 }
